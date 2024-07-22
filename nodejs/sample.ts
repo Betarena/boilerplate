9d144b208303beb09b59a308f08ea607a010a035
MigBash <20924663+migbash@users.noreply.github.com>
2024-07-22 02:04:45 +0200

    Create sample.ts

---
+++ nodejs/sample.ts
@@ -0,0 +1,119 @@
+// ╭──────────────────────────────────────────────────────────────────────────────────╮
+// │ 📌 High Order Overview                                                           │
+// ┣──────────────────────────────────────────────────────────────────────────────────┫
+// │ ➤ Code Format   // V.8.0                                                         │
+// │ ➤ Status        // 🔒 LOCKED                                                     │
+// │ ➤ Author(s)     // @migbash                                                      │
+// │ ➤ Maintainer(s) // @migbash                                                      │
+// │ ➤ Created on    // 2021-09-30                                                    │
+// ┣──────────────────────────────────────────────────────────────────────────────────┫
+// │ 📝 Description                                                                   │
+// ┣──────────────────────────────────────────────────────────────────────────────────┫
+// │ Betarena // TS.Module // <INSERT-type-module-scope>
+// │ :| <INSERT-general-module-description>
+// ╰──────────────────────────────────────────────────────────────────────────────────╯
+
+// #region ➤ 📦 Package Imports
+
+// #endregion ➤ 📦 Package Imports
+
+// #region ➤ 📌 VARIABLES
+
+// #endregion ➤ 📌 VARIABLES
+
+/**
+ * @author
+ *  @migbash
+ * @summary
+ *  - 🟦 HELPER
+ *  - 🟥 IMPORTANT
+ * @description
+ *  📝 <INSERT-general-method-description>
+ * @example
+ * [1] ─────────────────────────────────────────────────────────────────────────────
+ * exampleLogic
+ * (
+ *   value1: null,
+ *   value2: 'exampleString',
+ *   value3: false
+ * );
+ * ↳ DESCRIPTION :: <INSERT-specific-argument-combination-logic-execution>
+ * [2] ─────────────────────────────────────────────────────────────────────────────
+ * exampleLogic
+ * (
+ *   value1: 'exampleString1',
+ *   value2: 'exampleString2',
+ *   value3: true
+ * );
+ * ↳ DESCRIPTION :: <INSERT-specific-argument-combination-logic-execution>
+ * [X] ─────────────────────────────────────────────────────────────────────────────
+ * @param { string | null } value1
+ *  💠 **REQUIRED** <INSERT-description>
+ * @param { string } value2
+ *  💠 **REQUIRED** <INSERT-description>
+ * @param { boolean } [value3=false]
+ *  💠 **OPTIONAL** `(default=false)` <INSERT-description>
+ * @return { boolean }
+ *  📤 <INSERT-return-value>
+ */
+export function exampleLogic
+(
+  value1: string | null,
+  value2: string,
+  value3: boolean = false
+): boolean
+{
+  return true;
+}
+
+/**
+ * @description
+ * 📝 <INSERT-general-class-description>
+ */
+export class Example
+{
+  // ╭──────────────────────────────────────────────────────────────────────────────────╮
+  // │ 📌 │ VARIABLE(S)                                                                 │
+  // ╰──────────────────────────────────────────────────────────────────────────────────╯
+
+  /**
+   * @description
+   * 📝 <INSERT-general-variable-description>
+   */
+  variable1: string;
+
+  /**
+   * @description
+   *  📝 <INSERT-general-method-description>
+   */
+  constructor ()
+  {
+    // ...
+  }
+
+  // ╭──────────────────────────────────────────────────────────────────────────────────╮
+  // │ 📌 │ HELPER METHODS                                                              │
+  // ╰──────────────────────────────────────────────────────────────────────────────────╯
+
+  /**
+   * @author
+   *  @migbash
+   * @summary
+   *  - 🟦 HELPER
+   * @description
+   *  📝 <INSERT-general-method-description>
+   * @example
+   * [1] ─────────────────────────────────────────────────────────────────────────────
+   * exampleLogic();
+   * ↳ DESCRIPTION :: <INSERT-specific-argument-combination-logic-execution>
+   * [X] ─────────────────────────────────────────────────────────────────────────────
+   * @return { boolean }
+   *  📤 <INSERT-return-value>
+   */
+  exampleLogic
+  (
+  ): void
+  {
+    return;
+  }
+}
